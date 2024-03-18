528c81dcb9e8cdeb50594bc39eeac1ba17233d2a
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-03-18 10:04:33 +0530

    clean everything
    Restart again

--- old/include/ram/pc.h
+++
@@ -1,8 +0,0 @@
-struct process_control_block {
-	int state;
-	long counter;
-	int priority;
-};
-
-struct process_control_block *create_process(void);
-void destroy_process(struct process_control_block *task);
