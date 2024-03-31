8c6d152828c5fc9d8657062853590ab46e838f79
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-03-31 14:55:27 +0530

    fs_driver skeleton

---
+++ fs_driver.c
@@ -0,0 +1,8 @@
+/*
+ * Open or create a file.
+ */
+int open(char *filename, char *directory, char *partition)
+{
+	/* open file '/dev/sda2'
+	/* read i-node bit map for number of nodes to lookup for the filename */
+}
---
+++ fs_driver.h
@@ -0,0 +1,2 @@
+int open(char *filename, char *directory, char *partition);
+int create(char *filename, char *directory, char *partition);
