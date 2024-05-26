abc3c04bdd1184b5363413491d15dc5b6638cd0a
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-05-27 00:39:48 +0530

    bash: update interrupt code

--- isr.c
+++ isr.c
@@ -8,28 +8,29 @@ void isr_handler(registers_t regs) {
   // Print(L"recieved interrupt: ");
   //Print(L"I was here %x\n", regs.rbp);
   Print(L"rip=%x\n", regs.rip);
+  Print(L"error code=%d\n", regs.err_code);
   // Print(L"syscall #%d called!\n", regs.rax);
   Print(L"ISR #%d called!\n", regs.int_no);
   // for(;;) {}
 }
 
-void irq_handler(registers_t regs) {
-  if (regs.int_no != 0x20)
-    Print(L"recieved interrupt: 0x%x\n",regs.int_no);
-
-  if ((regs.int_no == 0x2f) || (regs.int_no == 0x2e)) {
-  }
-  if (regs.int_no >= 40) {
-    // outb(0xA0, 0x20);
-  }
-
-  //outb(0x20, 0x20);
-
-  if (interrupt_handlers[regs.int_no] != 0) {
-    isr_t handler = interrupt_handlers[regs.int_no];
-    handler(regs);
-  }
-}
+// void irq_handler(registers_t regs) {
+//   if (regs.int_no != 0x20)
+//     Print(L"recieved interrupt: 0x%x\n",regs.int_no);
+// 
+//   if ((regs.int_no == 0x2f) || (regs.int_no == 0x2e)) {
+//   }
+//   if (regs.int_no >= 40) {
+//     // outb(0xA0, 0x20);
+//   }
+// 
+//   //outb(0x20, 0x20);
+// 
+//   if (interrupt_handlers[regs.int_no] != 0) {
+//     isr_t handler = interrupt_handlers[regs.int_no];
+//     handler(regs);
+//   }
+// }
 
 void register_interrupt_handler(uint8_t n, isr_t handler) {
   interrupt_handlers[n] = handler;
