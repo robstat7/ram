1a6f7ce0b59b32fce1ed23bcac892334152ac5a7
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-05-27 02:13:08 +0530

    hello: comment out stack alignment and restore code

--- call_hello.asm
+++ call_hello.asm
@@ -7,10 +7,10 @@ extrn hello
 section '.text' executable
 
 pre_hello:
-	push rbp
-	mov rbp, rsp
-	and rsp, 0xfffffffffffffff0
+	; push rbp
+	; mov rbp, rsp
+	; and rsp, 0xfffffffffffffff0
 	call hello
-	mov rsp, rbp
-	pop rbp
+	; mov rsp, rbp
+	; pop rbp
 	ret
